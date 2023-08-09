22e540c6c6593dbb9990e2410faacd670b008d75
kalipero <kalimil.vavricka@gmail.com>
2023-08-09 17:48:12 +0200

    all

--- app/src/main/java/com/example/onearmedbandit/MainActivity.kt
+++ app/src/main/java/com/example/onearmedbandit/MainActivity.kt
@@ -14,10 +14,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.onearmedbandit.ui.theme.OneArmedBanditTheme
+import kotlinx.coroutines.*
+
 import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
@@ -34,75 +35,137 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+object GlobalData{
+    var currentChar by mutableStateOf('A')
+    var charList by  mutableStateOf(mutableListOf<Char>())
+}
 
 @Composable
 fun RandomCharGenerator() {
-    var currentChar by remember { mutableStateOf('A') }
-    var charList by remember { mutableStateOf(mutableListOf<Char>()) }
-    val context = LocalContext.current
 
 
+    var isRunning by remember { mutableStateOf(false) }
+    val scope = rememberCoroutineScope()
+    val myBackground = Color(android.graphics.Color.parseColor("#219EBC"))
+    val btn_background = Color(android.graphics.Color.parseColor("#FFB703"))
+    val result_bg = Color(android.graphics.Color.parseColor("#8ECAE6"))
+    val window_bg = Color(android.graphics.Color.parseColor("#FB8500"))
 
+    // Okno s random čísly
     Column(
-        modifier = Modifier.padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(100.dp)
+            .background(color = window_bg)
+        ,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+
+    ){
+            Text(
+                text = GlobalData.currentChar.toString(),
+                //style = MaterialTheme.typography.h5,
+                modifier = Modifier
+                    .padding(16.dp)
+
+            )
+
+
+
+      //  Spacer(modifier = Modifier.height(16.dp))
+
+
+
+    }
+
+    // Sloupec s buttony
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = myBackground)
+            .padding(top = 20.dp, bottom = 20.dp)
+        ,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Text(
-            text = currentChar.toString(),
-            style = MaterialTheme.typography.h5,
-            modifier = Modifier.padding(16.dp)
-        )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
 
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(
-            onClick = {
-                currentChar = ('A'..'Z').random()
-                charList.add(currentChar)
-                // Aktualizace stavu seznamu
-                charList = charList
-            }
         ) {
-            Text("Generate Random Char")
+            Button(
+                onClick = {
+                    isRunning = !isRunning
+                    if (isRunning){
+                        scope.launch {
+                            startRandomCharacterChange()
+                        }
+                    } else {
+                        isRunning = false
+                    }
+                },
+                modifier = Modifier,
+                colors = ButtonDefaults.buttonColors(backgroundColor = btn_background)
+
+            ) {
+                Text(text = if (isRunning) "Stop" else "Generate", color = myBackground)
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+            Button(
+                onClick = {
+                    GlobalData.charList.clear()
+
+                },
+                modifier = Modifier,
+                colors = ButtonDefaults.buttonColors(backgroundColor = btn_background)
+            ) {
+                Text("Clear", color = myBackground)
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+            Button(
+                onClick = {
+                    GlobalData.currentChar = ('A'..'Z').random()
+                    GlobalData.charList.add(GlobalData.currentChar)
+                    // Aktualizace stavu seznamu
+                    charList = GlobalData.charList
+                },
+                modifier = Modifier,
+                colors = ButtonDefaults.buttonColors(backgroundColor = btn_background)
+            ) {
+                Text("Sort it", color = myBackground)
+            }
         }
     }
+
+    // Sloupec s hodnotami
     Column(
         modifier = Modifier
             .fillMaxWidth()
             .verticalScroll(rememberScrollState())
-            .background(color = Color.Red)
+            .background(color = result_bg)
     ) {
-        charList.forEachIndexed { index, char ->
+        GlobalData.charList.forEachIndexed { index, char ->
             Text(text = "$index: $char")
         }
     }
 }
+suspend fun startRandomCharacterChange() {
+    while (true) {
+        delay(500) // Změna znaku každých 500 ms
+        val newChar = ('A'..'Z').random()
+        // Aktualizace stavu
+        GlobalData.currentChar = newChar
 
-
-@Composable
-fun CharDisplay(currentChar: Char) {
-    Text(
-        text = "Current Char: $currentChar",
-        style = MaterialTheme.typography.body1,
-        modifier = Modifier.padding(16.dp)
-    )
-}
-
-@Composable
-fun MyApp() {
-    var currentChar by remember { mutableStateOf('A') }
-
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        RandomCharGenerator()
-        CharDisplay(currentChar)
     }
 }
 
 @Preview
 @Composable
 fun PreviewMyApp() {
-    MyApp()
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        RandomCharGenerator()
+    }
 }
